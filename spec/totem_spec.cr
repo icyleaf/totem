5e7ca990fde609d31201f76ce31a73317b359046
icyleaf <icyleaf.cn@gmail.com>
2018-07-18 18:47:53 +0800

    test: typo

--- spec/totem_spec.cr
+++ spec/totem_spec.cr
@@ -12,85 +12,85 @@ private def env_raw
   load_fixture("sample.env")
 end
 
-private def json_spec_group(r)
-  r.get("name").as_s.should eq "Cake"
-  r.get("gluten_free").as_bool.should be_false
-  r.get("duty_free").as_bool(strict: false).should be_false
-  r.get("ppu").as_f.should eq 0.55
-  r.get("batters").as_h["batter"].as_a[0].as_h["type"].as_s.should eq "Regular"
+private def json_spec_group(t)
+  t.get("name").as_s.should eq "Cake"
+  t.get("gluten_free").as_bool.should be_false
+  t.get("duty_free").as_bool(strict: false).should be_false
+  t.get("ppu").as_f.should eq 0.55
+  t.get("batters").as_h["batter"].as_a[0].as_h["type"].as_s.should eq "Regular"
 end
 
-private def yaml_spec_group(r)
-  r.get("hacker").as_bool.should be_true
-  r.get("gender").as_bool(strict: false).should be_true
-  r.get("age").as_i.should eq 35
-  r.get("clothing").as_h["pants"].as_h["size"].as_s.should eq "large"
+private def yaml_spec_group(t)
+  t.get("hacker").as_bool.should be_true
+  t.get("gender").as_bool(strict: false).should be_true
+  t.get("age").as_i.should eq 35
+  t.get("clothing").as_h["pants"].as_h["size"].as_s.should eq "large"
 end
 
-private def env_spec_group(r)
-  r.get("blank").as_s.empty?.should be_true
-  r.get("str").as_s.should eq "foo"
-  r.get("STR_with_comments").as_s.should eq "bar"
-  r.get("STR_with_hash_symbol").as_s.should eq "abc#123"
-  r.get("int").as_i.should eq 42
-  r.get("float").as_f.should eq 33.3
-  r.get("BOOL_TRUE").as_i.should eq 1
-  r.get("BOOL_FALSE").as_i.should eq 0
-  r.get("BOOL_TRUE").as_bool?.should be_nil
-  r.get("BOOL_TRUE").as_bool(strict: false).should be_true
-  r.get("BOOL_FALSE").as_bool?.should be_nil
-  r.get("BOOL_FALSE").as_bool(strict: false).should be_false
+private def env_spec_group(t)
+  t.get("blank").as_s.empty?.should be_true
+  t.get("str").as_s.should eq "foo"
+  t.get("STR_with_comments").as_s.should eq "bar"
+  t.get("STR_with_hash_symbol").as_s.should eq "abc#123"
+  t.get("int").as_i.should eq 42
+  t.get("float").as_f.should eq 33.3
+  t.get("BOOL_TRUE").as_i.should eq 1
+  t.get("BOOL_FALSE").as_i.should eq 0
+  t.get("BOOL_TRUE").as_bool?.should be_nil
+  t.get("BOOL_TRUE").as_bool(strict: false).should be_true
+  t.get("BOOL_FALSE").as_bool?.should be_nil
+  t.get("BOOL_FALSE").as_bool(strict: false).should be_false
 end
 
 describe Totem do
   describe "#new" do
     it "should works as Totem::Reader" do
-      r = Totem.new
-      r.set_default "name", "foo"
-      r.get("name").as_s.should eq "foo"
+      t =  Totem.new
+      t.set_default "name", "foo"
+      t.get("name").as_s.should eq "foo"
 
-      r.set("name", "bar")
-      r.alias(alias_key: "key", key: "name")
-      r.get("name").as_s.should eq "bar"
-      r.get("key").as_s.should eq "bar"
+      t.set("name", "bar")
+      t.alias(alias_key: "key", key: "name")
+      t.get("name").as_s.should eq "bar"
+      t.get("key").as_s.should eq "bar"
     end
   end
 
   describe "#parse" do
     it "of json" do
-      r = Totem.parse json_raw, "json"
-      json_spec_group r
+      t = Totem.parse json_raw, "json"
+      json_spec_group t
     end
 
     it "of yaml" do
-      r = Totem.parse yaml_raw, "yaml"
-      yaml_spec_group r
+      t = Totem.parse yaml_raw, "yaml"
+      yaml_spec_group t
     end
 
     it "of env" do
-      r = Totem.parse env_raw, "env"
-      env_spec_group r
+      t = Totem.parse env_raw, "env"
+      env_spec_group t
     end
   end
 
   describe "#from_yaml" do
     it "should parse" do
-      r = Totem.from_yaml yaml_raw
-      yaml_spec_group r
+      t = Totem.from_yaml yaml_raw
+      yaml_spec_group t
     end
   end
 
   describe "#from_json" do
     it "should parse" do
-      r = Totem.from_json json_raw
-      json_spec_group r
+      t = Totem.from_json json_raw
+      json_spec_group t
     end
   end
 
   describe "#from_env" do
     it "should parse" do
-      r = Totem.from_env env_raw
-      env_spec_group r
+      t = Totem.from_env env_raw
+      env_spec_group t
     end
   end
 
@@ -98,39 +98,39 @@ describe Totem do
     describe "use json file" do
       it "without paths" do
         file = File.join(fixture_path, "config.json")
-        r = Totem.from_file file
-        json_spec_group r
+        t = Totem.from_file file
+        json_spec_group t
       end
 
       it "with paths" do
-        r = Totem.from_file "config.json", [".", "~/", fixture_path]
-        json_spec_group r
+        t = Totem.from_file "config.json", [".", "~/", fixture_path]
+        json_spec_group t
       end
     end
 
     it "use yaml file" do
       it "without paths" do
         file = File.join(fixture_path, "config.yaml")
-        r = Totem.from_file file
-        yaml_spec_group r
+        t = Totem.from_file file
+        yaml_spec_group t
       end
 
       it "with paths" do
-        r = Totem.from_file "config.yaml", [".", fixture_path, "~/"]
-        yaml_spec_group r
+        t = Totem.from_file "config.yaml", [".", fixture_path, "~/"]
+        yaml_spec_group t
       end
     end
 
     it "use env file" do
       it "without paths" do
         file = File.join(fixture_path, "sample.yaml")
-        r = Totem.from_file file
-        env_spec_group r
+        t = Totem.from_file file
+        env_spec_group t
       end
 
       it "with paths" do
-        r = Totem.from_file "sample.yaml", [".", fixture_path, "~/"]
-        env_spec_group r
+        t = Totem.from_file "sample.yaml", [".", fixture_path, "~/"]
+        env_spec_group t
       end
     end
   end
