8d07bc79e29a870825c0c6e85340ee2e5de29727
icyleaf <icyleaf.cn@gmail.com>
2018-09-21 17:30:13 +0800

    doc(remote_providers): update comments

--- src/totem/remote_providers.cr
+++ src/totem/remote_providers.cr
@@ -3,24 +3,33 @@ module Totem
   module RemoteProviders
     @@providers = {} of String => Adapter.class
 
+    # Registe remote provider with name
+    #
+    # ```
+    # Totem::ConfigTypes.register_adapter("redis", Totem::RemoteProvider::Redis)
+    # ```
     def self.register_adapter(name : String, adapter : Adapter.class)
       @@providers[name] = adapter
     end
 
+    # Returns the value for the key given by *key*.
     def self.[](name : String)
       @@instance[name]
     end
 
+    # Returns a new `Array` with all the keys.
     def self.keys
       @@providers.keys
     end
 
+    # Returns `true` when key given by *key* exists, otherwise `false`.
     def self.has_key?(key)
       keys.includes?(key)
     end
 
     @@instance = {} of String => Adapter
 
+    # Connect remote provider with options
     def self.connect(name : String, **options)
       cls = @@providers[name]
       @@instance[name] = cls.new(**options)
